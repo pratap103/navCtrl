154c1cbaa45734df556b3fad18d4f1d85d39632d
Pratap Pandit <pratap.pandit88@gmail.com>
2016-08-01 15:52:34 -0400

    added rest of product thumbnails, app loading slowly

--- NavCtrl/DataAccessObject.m
+++ NavCtrl/DataAccessObject.m
@@ -28,22 +28,22 @@ static DataAccessObject* _sharedDataAccessObject = nil;
     companyApple.products = [NSMutableArray arrayWithObjects:iPad, iPodTouch, iPhone, nil];
     
     Company *companySamsung = [[Company alloc] initWithName:@"Samsung mobile devices" stockSymbol:@"SSNLF" imageURL:@"http://www.brandsoftheworld.com/sites/default/files/styles/logo-thumbnail/public/0019/9958/brand.gif?itok=_aI8_zLB"];
-//    Product *galaxyS7 = [[Product alloc] initWithName:@"Galaxy S7" ];
-//    Product *galaxyNote = [[Product alloc] initWithName:@"Galaxy Note"];            //samsung products
-//    Product *galaxyTab = [[Product alloc] initWithName:@"Galaxy Tab"];
-//    companySamsung.products = [NSMutableArray arrayWithObjects:galaxyS7, galaxyNote, galaxyTab, nil];
+    Product *galaxyS7 = [[Product alloc] initWithName:@"Galaxy S7" productURL:@"http://www.samsung.com/us/mobile/galaxy-tab/" productImageURL:@"http://www.samsung.com/us/explore/galaxy-s7-features-and-specs/dist/assets/img/module1/desktop/intro.jpg"];
+    Product *galaxyNote = [[Product alloc] initWithName:@"Galaxy Note" productURL:@"http://www.samsung.com/us/mobile/galaxy-note/" productImageURL:@"http://www.samsung.com/global/galaxy/galaxy-note5/images/galaxy-note5_gallery_right-perspective_pinkgold_s3.png"];            //samsung products
+    Product *galaxyTab = [[Product alloc] initWithName:@"Galaxy Tab" productURL:@"http://www.samsung.com/us/explore/galaxy-s7-features-and-specs/" productImageURL:@"http://www.samsung.com/global/galaxy/galaxy-tab-s2/images/galaxy-tab-s2_gallery_front_black.png"];
+    companySamsung.products = [NSMutableArray arrayWithObjects:galaxyS7, galaxyNote, galaxyTab, nil];
     
     Company *companyBlackberry = [[Company alloc] initWithName:@"Blackberry mobile devices" stockSymbol:@"BBRY" imageURL: @"http://dolphinradio.org/wp-content/uploads/2014/07/BlackBerry-Logo.png"];
-//    Product *classic = [[Product alloc] initWithName:@"Classic"];
-//    Product *leap = [[Product alloc] initWithName:@"Leap"];                         //blackberry products
-//    Product *passport = [[Product alloc] initWithName:@"Passport"];
-//    companyBlackberry.products =  [NSMutableArray arrayWithObjects:classic, leap, passport, nil];
+    Product *classic = [[Product alloc] initWithName:@"Classic" productURL:@"http://us.blackberry.com/smartphones/blackberry-classic/overview.html" productImageURL:@"http://us.blackberry.com/content/dam/blackberry-com/images/smartphones/2016_Classic_Overview/classic-colour-variations_black.png"];
+    Product *leap = [[Product alloc] initWithName:@"Leap" productURL:@"http://us.blackberry.com/smartphones/blackberry-leap/overview.html" productImageURL:@"https://crackberry.com/sites/crackberry.com/files/styles/large/public/article_images/2015/03/blackberry-leap-back-hand-hero.jpg?itok=zpPbqOvJ"];                         //blackberry products
+    Product *passport = [[Product alloc] initWithName:@"Passport" productURL:@"http://us.blackberry.com/smartphones/blackberry-passport/overview.html" productImageURL:@"http://us.blackberry.com/content/dam/blackberry-com/images/smartphones/2016_Passport_Overview/passport-colours-silver.png/jcr:content/renditions/original"];
+    companyBlackberry.products =  [NSMutableArray arrayWithObjects:classic, leap, passport, nil];
     
     Company *companyNexus = [[Company alloc] initWithName:@"Nexus mobile devices" stockSymbol:@"GOOG" imageURL:@"https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/X_from_Nexus_logo.svg/120px-X_from_Nexus_logo.svg.png"];
-//    Product *n5X = [[Product alloc] initWithName:@"5X"];
-//    Product *n6P = [[Product alloc] initWithName:@"6P"];                            //nexus products
-//    Product *n9 = [[Product alloc] initWithName:@"9"];
-//    companyNexus.products = [NSMutableArray arrayWithObjects:n5X, n6P, n9, nil];
+    Product *n5X = [[Product alloc] initWithName:@"5X" productURL:@"https://www.google.com/nexus/5x/" productImageURL:@"https://lh3.googleusercontent.com/P1_mBNk543linbcL0AtazL5Kr9Te0v7egH-uxhWoddVuSFeH-jfAt9dYB4ELnElYNw"];
+    Product *n6P = [[Product alloc] initWithName:@"6P" productURL:@"https://store.google.com/product/nexus_6p" productImageURL:@"https://lh3.googleusercontent.com/IW5kkESaYgPvqetsNKtwq_0HZdVJRjWwC5BXeAi_1zPoFmXD8PmkmK7jDH_ZDgaytSqo"];                            //nexus products
+    Product *n9 = [[Product alloc] initWithName:@"9" productURL:@"https://www.google.com/nexus/9/" productImageURL:@"https://www.google.com/nexus/images/nexus9/N9-grid1-1600.jpg"];
+    companyNexus.products = [NSMutableArray arrayWithObjects:n5X, n6P, n9, nil];
     
     
     
